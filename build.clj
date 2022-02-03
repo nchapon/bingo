521a98d21b130702e1a78e2b7346788fa40bb943
nchapon <nchapon@gmail.com>
2022-02-03 18:31:46 +0100

    Build with tools.build

---
+++ build.clj
@@ -0,0 +1,18 @@
+(ns build
+  (:refer-clojure :exclude [test])
+  (:require
+   [org.corfield.build :as bb]))
+
+(def lib 'isedia/bingo)
+(def version "0.1.0-SNAPSHOT")
+(def main 'bingo.core)
+
+(defn test "Run the tests." [opts]
+  (bb/run-tests opts))
+
+(defn ci "Run the CI pipeline of tests (and build the uberjar)." [opts]
+  (-> opts
+      (assoc :lib lib :version version :main main)
+      (bb/run-tests)
+      (bb/clean)
+      (bb/uber)))
