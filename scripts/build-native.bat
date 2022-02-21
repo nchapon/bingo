f00e55a0ed54c8c6812e7a6fbca83e1a6b6fc274
nchapon <nchapon@gmail.com>
2022-02-21 18:15:06 +0100

    Graal VM build

---
+++ scripts/build-native.bat
@@ -0,0 +1,13 @@
+cd %~dp0
+
+native-image --report-unsupported-elements-at-runtime ^
+    --no-fallback ^
+    --verbose ^
+    --initialize-at-build-time ^
+    --native-image-info ^
+    --initialize-at-run-time=org.httpkit.client.ClientSslEngineFactory\\$SSLHolder ^
+    --enable-url-protocols=http,https ^
+    -jar ../target/bingo-0.1.0-SNAPSHOT.jar ^
+    -H:+ReportExceptionStackTraces ^
+    -H:ReflectionConfigurationFiles=../resources/META-INF/native-image/logging.json ^ 
+    -H:Name=./target/bingo
